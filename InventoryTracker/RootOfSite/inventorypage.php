f16c4cada6d45337f05fe00ecb3ef86aecdb5ac6
John Chandler <chandler6830@student.cptc.edu>
2017-05-07 20:36:12 -0700

    Started making the desplay page... wanted to pull the commit in from Oddesy so I had to commit my work thus far

---
+++ InventoryTracker/RootOfSite/inventorypage.php
@@ -0,0 +1,30 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+	<meta charset="utf-8" />
+</head>
+<body>
+	<?php
+		$servername = "http://96.81.150.5/";
+		$username = "beekerINVTRACKER";
+		$password = "NpgqdulHOhLARGmx";
+		$dbname = "InventoryTrackerCptc";
+
+		//Create the Connection
+		$con = new mysqli($servername, $username, $password, $dbname);
+		
+		if ($conn->connect_error) {
+    		die("Connection failed: " . $conn->connect_error);
+		} 
+
+		$sql = "SELECT 
+					id,
+					firstname,
+					lastname 
+				FROM MyGuests";
+		$result = $conn->query($sql);
+		
+	?>
+</body>
+</html>
