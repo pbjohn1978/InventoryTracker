c5a0ad3cea6855860da5a4746b9a15441fddc63e
chand <pbjohn1978@yahoo.com>
2017-06-18 02:59:17 -0700

    Made a new page that will add a new store to the DB

---
+++ InventoryTracker/RootOfSite/newStoreGet.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<form action="newStorePost.php" method="post">
+    	<fieldset>
+        	<br><br><label for="storeName">New Store</label>
+        	<input type="text" name="storeName"><br><br>
+            <input type="submit">
+        </fieldset>
+    </form>
+</body>
+</html>
---
+++ InventoryTracker/RootOfSite/newStorePost.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<?php
+		$servername = "";
+		$username = "";
+		$password = "";
+		$dbname = "InventoryTrackerCptc";
+		$newStore = $_POST["storeName"];
+		$sql = "INSERT INTO `Store`(`StoreName`) VALUES ('".$newStore."')";
+		$outputString = "0 Locations Added.";
+		
+		//Create the Connection
+		$con = mysqli_connect($servername, $username, $password, $dbname);
+		if (!$con) {
+			die("Connection failed: ".mysqli_connect_error());
+		}
+		
+		//Run the Query...
+		$result = mysqli_query($con, $sql);
+		
+		//Process the results to a string for output
+		if ( $result ) {
+			$outputString = "1 New Store Added '".$newStore."'";
+		}
+		
+		echo $outputString."<br>";
+		
+		//close the connection to server
+		$con->close();
+	?>
+</body>
+</html>
